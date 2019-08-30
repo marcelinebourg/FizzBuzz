1802b3664dabd2621f3aab1e1226498360d062f5
s198007 <s198007@LAF-8CC59B>
2019-08-30 09:31:16 -0500

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            while (count <= 100)
+            {
+
+                if (count % 3 == 0 && count % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (count % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (count % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(count);
+                }
+                count++;
+                
+            }
+            Console.ReadKey();
+            
+
+        }
+        
+
+        
+    }
+}
